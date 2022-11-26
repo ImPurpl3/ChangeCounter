6999b9274e9add97702d3be41b60650eb3103230
ImPurpl3 <cjhilderman@gmail.com>
2022-11-25 21:48:31 -0600

    gitignore

--- ChangeCounter/Form1.Designer.cs
+++ ChangeCounter/Form1.Designer.cs
@@ -422,7 +422,7 @@
             // FileDateLabel
             // 
             this.FileDateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FileDateLabel.Location = new System.Drawing.Point(17, 416);
+            this.FileDateLabel.Location = new System.Drawing.Point(14, 416);
             this.FileDateLabel.Name = "FileDateLabel";
             this.FileDateLabel.Size = new System.Drawing.Size(533, 26);
             this.FileDateLabel.TabIndex = 34;
